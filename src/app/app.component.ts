699b5f7354837eb66d1b609896ea41019fa897dc
Rodrigo <rodrigo.nunes-55@hotmail.com>
2023-11-21 22:17:59 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = []; //classe transformada em array?
+  public title: string = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com o Cachorro', false));
+    this.todos.push(new Todo(2, 'Ir ao Supermercado', false));
+    this.todos.push(new Todo(3, 'Cortar o Cabelo', true));
+    
+  }
+
+  remove(remover: Todo){
+    const index = this.todos.indexOf(remover);
+    console.log(index);
+    if(index !== -1){
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(){
+
+  }
+
+  maskAsUndone(){
+
+  }
 }
